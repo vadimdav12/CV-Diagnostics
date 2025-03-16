358eb8e91451ef847e86e4cc67b5fe533c9981ae
CV-Diagnostics <dzida.vadim@yandex.ru>
2025-03-16 17:04:20 +0300

    Stable-Version-v1. add login

--- my-project/src/main/java/com/example/controller/HomeController.java
+++ my-project/src/main/java/com/example/controller/HomeController.java
@@ -1,8 +1,9 @@
-package com.example.demo;
+package com.example.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
 
 @Controller
 public class HomeController {
